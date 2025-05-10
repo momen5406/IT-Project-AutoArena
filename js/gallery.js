f2d48c4c3806779f009a0d1ca8e06777ceefab01
Momen Hussein <morenocoder@gmail.com>
2025-05-10 21:13:21 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,9 @@ const searchInput = document.getElementById('search');
 const cars = document.querySelectorAll('.car-card');
 
 searchInput.addEventListener('input', () => {
-    const query = searchInput.value.toLowerCase();
+    const searchName = searchInput.value.toLowerCase();
     cars.forEach(car => {
         const name = car.dataset.name.toLowerCase();
-        car.style.display = name.includes(query) ? 'block' : 'none';
+        car.style.display = name.includes(searchName) ? 'block' : 'none';
     });
 });
